50af0fe0bfc3a8bac78ad2bace787edf8c9b58a0
v.pakhomia <v.pakhomia@banki.ru>
2023-01-21 12:25:28 +0300

    refaktor

--- src/test/java/test/TestJava.java
+++ src/test/java/test/TestJava.java
@@ -14,52 +14,52 @@ public class TestJava {
         List<String> list = new ArrayList<>();
         
         byte b = Byte.MAX_VALUE;
-        list.add("MaxByte - " + b);
+        list.add("MaxByte - b = " + b);
         byte b2 = Byte.MIN_VALUE;
-        list.add("MinByte - " + b2);
+        list.add("MinByte - b2 = " + b2);
                 
         short s = Short.MAX_VALUE;
-        list.add("MaxShort - " + s);
+        list.add("MaxShort - s = " + s);
         short s2 = Short.MIN_VALUE;
-        list.add("MinShort - " + s2);
+        list.add("MinShort - s2 = " + s2);
                 
         int i = Integer.MAX_VALUE;
-        list.add("MaxInt - " + i);
+        list.add("MaxInt - i = " + i);
         int i2 = Integer.MIN_VALUE;
-        list.add("MinInt - " + i2);
+        list.add("MinInt - i2 = " + i2);
                 
         long l = Long.MAX_VALUE;
-        list.add("MinLong - " + l);
+        list.add("MinLong - l = " + l);
         long l2 = Long.MIN_VALUE;
-        list.add("MinLong - " + l2);
+        list.add("MinLong - l2 = " + l2);
                 
         float f = Float.MAX_VALUE;
-        list.add("MaxFloat - " + f);
+        list.add("MaxFloat - f = " + f);
         float f2 = Float.MIN_VALUE;
-        list.add("MinFloat - " + f2);
+        list.add("MinFloat - f2 = " + f2);
                 
         double d = Double.MAX_VALUE;
-        list.add("MaxDouble - " + d);
+        list.add("MaxDouble - d = " + d);
         double d2 = Double.MIN_VALUE;
-        list.add("MinDouble - " + d2);
+        list.add("MinDouble - d2 = " + d2);
                 
         char c = Character.MAX_VALUE;
-        list.add("MaxChar - " + c);
+        list.add("MaxChar - c = " + c);
         char c2 = Character.MIN_VALUE;
-        list.add("MinChar - " + c2);
+        list.add("MinChar - c2 = " + c2);
                 
         boolean bol = true;
-        list.add("boolean - " + bol);
+        list.add("boolean - bol = " + bol);
         boolean bol2 = false;
-        list.add("boolean - " + bol2);
+        list.add("boolean - bol2 = " + bol2);
         
         list.forEach(System.out::println);
 
         System.out.println("\nПереполнение");
-        System.out.println("Byte в плюс" + ++b);
-        System.out.println("Byte в минус" + --b2);
-        System.out.println("Long в плюс" + ++l);
-        System.out.println("Long в минус" + --l2);
+        System.out.println("Byte в плюс (b+1) - " + ++b);
+        System.out.println("Byte в минус (b2-1) - " + --b2);
+        System.out.println("Long в плюс (l+1) - " + ++l);
+        System.out.println("Long в минус (l2-1) - " + --l2);
 
         System.out.println("\nРазные типы");
         double d3 = 100.10;
